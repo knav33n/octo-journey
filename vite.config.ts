f703a623ed968d39704a2513f7f1fc7b7879b929
knav33n <13643224+knav33n@users.noreply.github.com>
2024-05-17 18:12:58 +0530

    testing: api url

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => {
-  console.log(command)
-  if (command === "serve") {
-    return {
-      plugins: [react()],
-      server: {
-        proxy: {
-          "/v1": "http://localhost:8800/api/",
-        },
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/v1": "http://3.77.150.166:8800/api/",
       },
-    };
-  } else {
-    return {
-      plugins: [react()],
-      server: {
-        proxy: {
-          "/v1": "http://3.77.150.166:8800/api/",
-        },
-      },
-    };
-  }
+    },
+  };
 });
