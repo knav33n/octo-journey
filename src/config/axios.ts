bcfee49615439e08770d8ce00bcfd1db502744e2
knav33n <13643224+knav33n@users.noreply.github.com>
2024-05-20 15:27:44 +0530

    fix: add header config

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -6,6 +6,7 @@ axios.interceptors.request.use(
   function (config) {
     config.baseURL = process.env.REACT_APP_API_URL;
     config.headers["Accept"] = "application/json";
+    config.withCredentials = true;
     return config;
   },
   function (error) {
